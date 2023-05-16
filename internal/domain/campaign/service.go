463458e97afa6d970660cf1166f420f459de0360
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-16 16:12:09 -0300

    :test_tube: test: service tests

--- internal/domain/campaign/service.go
+++ internal/domain/campaign/service.go
@@ -1,11 +1,17 @@
 package campaign
 
-import "sendify/internal/contract"
+import (
+	"sendify/internal/contract"
+)
 
 type Service struct {
 	Repository Repository
 }
 
-func (s *Service) Create(newCampaign contract.NewCampaign) error {
-	return nil
+func (s *Service) Create(newCampaign contract.NewCampaign) (string, error) {
+	campaign, _ := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+
+	s.Repository.Save(campaign)
+
+	return campaign.ID, nil
 }
